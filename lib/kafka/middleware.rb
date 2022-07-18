55e20ce176206a65c2e4fb8448d8f938e2e63910
Alex Pylko <alexpylko@gmail.com>
2022-07-18 16:22:22 +0200

    Add gem mock

---
+++ lib/kafka/middleware.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'middleware/version'
+
+module Kafka
+  module Middleware
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
